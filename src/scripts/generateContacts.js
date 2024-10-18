bc99e9a8c773562c586a74191f5a4b03ef9003b4
CaptainKirioto <mrg.zhi@gmail.com>
2024-10-18 13:42:05 +0200

    app finished

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  const newContacts = Array(number).fill().map(createFakeContact);
+  const allContacts = [...contacts, ...newContacts];
+  await writeContacts(allContacts);
+};
 
 generateContacts(5);
